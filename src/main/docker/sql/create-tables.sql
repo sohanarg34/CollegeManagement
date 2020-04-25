a3da00e6a48f102a918b7652e5fe77ed10ea8d22
sohanarg34 <sohanarg34@yahoo.co.in>
2020-04-25 18:03:17 -0400

    added uuid column

--- src/main/docker/sql/create-tables.sql
+++ src/main/docker/sql/create-tables.sql
@@ -2,24 +2,30 @@ use college_management;
 
 CREATE TABLE college (
   id INT NOT NULL AUTO_INCREMENT,
+  college_uuid VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE(college_uuid)
 );
 
 CREATE TABLE department (
-  college_id INT NOT NULL,
-  department_id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
+  department_uuid VARCHAR(255) NOT NULL,
+  college_uuid VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (department_id),
-  FOREIGN KEY (college_id) REFERENCES college(id)
+  PRIMARY KEY (id),
+  UNIQUE(department_uuid),
+  FOREIGN KEY (college_uuid) REFERENCES college(college_uuid)
 );
 
 CREATE TABLE student (
-  student_id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
+  student_id VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (student_id),
-  FOREIGN KEY (department_id) REFERENCES department(department_id)
+  department_uuid VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE(student_id),
+  FOREIGN KEY (department_uuid) REFERENCES department(department_uuid)
 );
